4b7ce1f2f158d4a55a8052178132bf702c8d3f2f
hoangcnttbkdn <hoangcnnttbkdn@gmail.com>
2022-11-09 08:30:40 -0800

    1

--- asset-transfer-basic/chaincode-javascript/lib/assetTransfer.js
+++ asset-transfer-basic/chaincode-javascript/lib/assetTransfer.js
@@ -15,48 +15,20 @@ class AssetTransfer extends Contract {
 
     async InitLedger(ctx) {
         const assets = [
-            {
-                ID: 'asset1',
-                Color: 'blue',
-                Size: 5,
-                Owner: 'Tomoko',
-                AppraisedValue: 300,
-            },
-            {
-                ID: 'asset2',
-                Color: 'red',
-                Size: 5,
-                Owner: 'Brad',
-                AppraisedValue: 400,
-            },
-            {
-                ID: 'asset3',
-                Color: 'green',
-                Size: 10,
-                Owner: 'Jin Soo',
-                AppraisedValue: 500,
-            },
-            {
-                ID: 'asset4',
-                Color: 'yellow',
-                Size: 10,
-                Owner: 'Max',
-                AppraisedValue: 600,
-            },
-            {
-                ID: 'asset5',
-                Color: 'black',
-                Size: 15,
-                Owner: 'Adriana',
-                AppraisedValue: 700,
-            },
-            {
-                ID: 'asset6',
-                Color: 'white',
-                Size: 15,
-                Owner: 'Michel',
-                AppraisedValue: 800,
-            },
+            // {
+            //     ID: '187238',
+            //     Fullname: 'Khanh Idol',
+            //     DateOfBirth: '06/06/2001',
+            //     Certificate: 'BANG TOT NGHIEP DAI HOC',
+            //     Speciality: 'Cong nghe thong tin',
+            //     GraduationYear: '2019',
+            //     School: 'Dai hoc BKDN',
+            //     Rank: 'Gioi',
+            //     ModeOfStudy: 'Chinh quy',
+            //     RegNo: '34567',
+            //     UrlImage: 'abc.com/a.jpeg',
+            // },
+            
         ];
 
         for (const asset of assets) {
@@ -70,7 +42,7 @@ class AssetTransfer extends Contract {
     }
 
     // CreateAsset issues a new asset to the world state with given details.
-    async CreateAsset(ctx, id, color, size, owner, appraisedValue) {
+    async CreateAsset(ctx, id, fullname, dateofbirth, certificate, speciality, graduationyear, school, rank, modeofstudy, regno, urlimage) {
         const exists = await this.AssetExists(ctx, id);
         if (exists) {
             throw new Error(`The asset ${id} already exists`);
@@ -78,10 +50,16 @@ class AssetTransfer extends Contract {
 
         const asset = {
             ID: id,
-            Color: color,
-            Size: size,
-            Owner: owner,
-            AppraisedValue: appraisedValue,
+            Fullname: fullname,
+            DateOfBirth: dateofbirth,
+            Certificate: certificate,
+            Speciality: speciality,
+            GraduationYear: graduationyear,
+            School: school,
+            Rank: rank,
+            ModeOfStudy: modeofstudy,
+            RegNo: regno,
+            UrlImage: urlimage,
         };
         //we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
         await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(asset))));
@@ -98,7 +76,7 @@ class AssetTransfer extends Contract {
     }
 
     // UpdateAsset updates an existing asset in the world state with provided parameters.
-    async UpdateAsset(ctx, id, color, size, owner, appraisedValue) {
+    async UpdateAsset(ctx, id, fullname, dateofbirth, certificate, speciality, graduationyear, school, rank, modeofstudy, regno, urlimage) {
         const exists = await this.AssetExists(ctx, id);
         if (!exists) {
             throw new Error(`The asset ${id} does not exist`);
@@ -107,10 +85,16 @@ class AssetTransfer extends Contract {
         // overwriting original asset with new asset
         const updatedAsset = {
             ID: id,
-            Color: color,
-            Size: size,
-            Owner: owner,
-            AppraisedValue: appraisedValue,
+            Fullname: fullname,
+            DateOfBirth: dateofbirth,
+            Certificate: certificate,
+            Speciality: speciality,
+            GraduationYear: graduationyear,
+            School: school,
+            Rank: rank,
+            ModeOfStudy: modeofstudy,
+            RegNo: regno,
+            UrlImage: urlimage,
         };
         // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
         return ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(updatedAsset))));
@@ -132,15 +116,15 @@ class AssetTransfer extends Contract {
     }
 
     // TransferAsset updates the owner field of asset with given id in the world state.
-    async TransferAsset(ctx, id, newOwner) {
-        const assetString = await this.ReadAsset(ctx, id);
-        const asset = JSON.parse(assetString);
-        const oldOwner = asset.Owner;
-        asset.Owner = newOwner;
-        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(asset))));
-        return oldOwner;
-    }
+    // async TransferAsset(ctx, id, newOwner) {
+    //     const assetString = await this.ReadAsset(ctx, id);
+    //     const asset = JSON.parse(assetString);
+    //     const oldOwner = asset.Owner;
+    //     asset.Owner = newOwner;
+    //     // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
+    //     await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(asset))));
+    //     return oldOwner;
+    // }
 
     // GetAllAssets returns all assets found in the world state.
     async GetAllAssets(ctx) {
